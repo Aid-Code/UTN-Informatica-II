653a57d500259140249c7c1a457500f2a4c0e5ac
Aiden Maid Perez <aiden.maidp@gmail.com>
2026-09-11 10:27:33 -0300

    TPO Creado

---
+++ MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.cpp
@@ -0,0 +1,18 @@
+/*
+ * HCSR04.cpp
+ *
+ *  Created on: 11 sep. 2026
+ *      Author: abril
+ */
+
+#include "HCSR04.h"
+
+HC_SR04::HC_SR04() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HC_SR04::~HC_SR04() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.h
@@ -0,0 +1,17 @@
+/*
+ * HCSR04.h
+ *
+ *  Created on: 11 sep. 2026
+ *      Author: Aiden
+ */
+
+#ifndef HCSR04_H_
+#define HCSR04_H_
+
+class HC_SR04
+{
+public:
+	HC_SR04();
+};
+
+#endif /* HCSR04_H_ */
