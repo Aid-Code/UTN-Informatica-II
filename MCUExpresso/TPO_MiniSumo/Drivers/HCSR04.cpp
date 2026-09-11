ad8c98248e7da16665ba42455ba55373997f7d86
Aiden Maid Perez <aiden.maidp@gmail.com>
2026-09-11 12:20:26 -0300

    HC-SR04

--- MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.cpp
+++ MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.cpp
@@ -2,17 +2,25 @@
  * HCSR04.cpp
  *
  *  Created on: 11 sep. 2026
- *      Author: abril
+ *      Author: Aiden
  */
 
 #include "HCSR04.h"
 
-HC_SR04::HC_SR04() {
-	// TODO Auto-generated constructor stub
+HC_SR04::HC_SR04(uint8_t _pin_trig, uint8_t _port_trig, uint8_t _pin_echo):pin_trig(_pin_trig),port_trig(_port_trig),pin_echo(_pin_echo)
+{
+	trig(port_trig, pin_trig, gpio::SALIDA);
 
+	CTIMER |= 1;
+	CTIMER->PR = (29*5);
+
+	CTIMER->MCR |= 1; // Match para el pulso del TRIG
+	CTIMER->MR0 = 4;
+
+	CTIMER->CCR |= 1; // Capture para medir el tiempo del pulso ECHO
 }
 
-HC_SR04::~HC_SR04() {
-	// TODO Auto-generated destructor stub
+HC_SR04::getDistancia(void)
+{
+	return distancia;
 }
-
--- MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.h
+++ MCUExpresso/TPO_MiniSumo/Drivers/HCSR04.h
@@ -8,10 +8,20 @@
 #ifndef HCSR04_H_
 #define HCSR04_H_
 
+#include <gpio.h>
+
+#define CTIMER (*((uint32_t*)0x4003 8000))
+
 class HC_SR04
 {
 public:
-	HC_SR04();
+	HC_SR04(uint8_t _pin_trig, uint8_t _port_trig, uint8_t _pin_echo);
+	uint32_t getDistancia(void);
+
+private:
+	gpio trig;
+	uint8_t pin_trig, port_trig, pin_echo;
+	uint32_t distancia, tiempo;
 };
 
 #endif /* HCSR04_H_ */
