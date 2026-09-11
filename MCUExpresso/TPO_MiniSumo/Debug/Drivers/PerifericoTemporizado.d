2d6edca7090dfb4cc9f781acaf3807af0da5f5eb
Dona <francodonadio2005@gmail.com>
2026-09-11 11:43:41 -0300

    putita

---
+++ MCUExpresso/TPO_MiniSumo/Debug/Drivers/PerifericoTemporizado.d
@@ -0,0 +1,5 @@
+Drivers/PerifericoTemporizado.o Drivers/PerifericoTemporizado.d: \
+ ../Drivers/PerifericoTemporizado.cpp ../Drivers/PerifericoTemporizado.h \
+ C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Firmware/LPC845.h
+../Drivers/PerifericoTemporizado.h:
+C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Firmware/LPC845.h:
---
+++ MCUExpresso/TPO_MiniSumo/Debug/Drivers/Uart.d
@@ -0,0 +1,5 @@
+Drivers/Uart.o Drivers/Uart.d: ../Drivers/Uart.cpp \
+ C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Drivers/Uart.h \
+ C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Firmware/LPC845.h
+C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Drivers/Uart.h:
+C:\Users\Franko\ Dona\Documents\GitHub\UTN-Informatica-II\MCUExpresso\TPO_MiniSumo\Firmware/LPC845.h:
